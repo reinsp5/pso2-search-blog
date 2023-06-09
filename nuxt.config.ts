9f1269604f85f02b12528f56957ff182659c7a52
DESKTOP-2HGUL02\reinsp5 <sk.reinsp5@gmail.com>
2023-06-09 23:42:43 +0900

    環境変数読み取り設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 import vuetify from "vite-plugin-vuetify";
 
+const {
+  NUXT_PUBLIC_FIREBASE_API_KEY,
+  NUXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  NUXT_PUBLIC_FIREBASE_PROJECT_ID,
+  NUXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  NUXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  NUXT_PUBLIC_FIREBASE_APP_ID,
+  NUXT_CLOUDFLARE_IMAGES_KEY,
+} = process.env;
+
 export default defineNuxtConfig({
   ssr: false,
   modules: ["nuxt-icons"],
@@ -31,16 +41,14 @@ export default defineNuxtConfig({
     },
   ],
   runtimeConfig: {
-    minioAccessKey: "",
-    minioAccessSecret: "",
-    cloudflareImagesKey: "",
+    cloudflareImagesKey: NUXT_CLOUDFLARE_IMAGES_KEY || "",
     public: {
-      firebaseApiKey: "",
-      firebaseAuthDomain: "",
-      firebaseProjectId: "",
-      firebaseStorageBucket: "",
-      firebaseMessagingSenderId: "",
-      firebaseAppId: "",
+      firebaseApiKey: NUXT_PUBLIC_FIREBASE_API_KEY || "",
+      firebaseAuthDomain: NUXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "",
+      firebaseProjectId: NUXT_PUBLIC_FIREBASE_PROJECT_ID || "",
+      firebaseStorageBucket: NUXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "",
+      firebaseMessagingSenderId: NUXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "",
+      firebaseAppId: NUXT_PUBLIC_FIREBASE_APP_ID || "",
       firebaseMeasurementId: "",
     },
   },
