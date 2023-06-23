0764c0f707fdcf8ab95be7688aac5f6378f8788d
DESKTOP-2HGUL02\reinsp5 <sk.reinsp5@gmail.com>
2023-06-23 23:19:26 +0900

    no message

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,7 +23,7 @@ export default defineNuxtConfig({
   },
   routeRules: {
     "/__/auth/**": {
-      proxy: { to: "https://pso2-items-database.firebaseapp.com" },
+      redirect: { to: "https://pso2-items-database.firebaseapp.com" },
     },
   },
   build: {
