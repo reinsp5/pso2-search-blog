3c039f96dded7a5ddff4e30b30afffb653f143ff
DESKTOP-2HGUL02\reinsp5 <sk.reinsp5@gmail.com>
2023-06-28 23:52:11 +0900

    トップページのマージン削除

--- pages/index.vue
+++ pages/index.vue
@@ -19,7 +19,7 @@ const { data } = useMicroCMSGetList<Blog>({
 
 <template>
   <v-container>
-    <v-row justify="center" class="w-75 mx-auto">
+    <v-row justify="center" class="w-md-75 mx-auto">
       <v-col cols="12" v-for="blog in data?.contents" :key="blog.id">
         <v-card variant="flat" :to="`/blog/${blog.id}`" v-ripple>
           <v-row class="my-4">
@@ -37,14 +37,14 @@ const { data } = useMicroCMSGetList<Blog>({
             <v-col>
               <v-row no-gutters>
                 <v-col cols="12">
-                  <span class="text-h6 text-md-h5">
+                  <span class="text-h6 text-md-h5 font-weight-bold">
                     <NuxtLink :to="`/blog/${blog.id}`">
                       {{ blog.title }}
                     </NuxtLink>
                   </span>
                 </v-col>
                 <v-col cols="12" class="text-caption">
-                  {{ blog.publishedAt ?? blog.createdAt }}
+                  公開日：{{ blog.publishedAt ? new Date(blog.publishedAt).toLocaleDateString() : new Date(blog.createdAt).toLocaleDateString() }}
                 </v-col>
                 <v-col class="pr-8 py-4" cols="12">
                   {{ blog.description }}
